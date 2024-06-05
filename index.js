519895cc379afa13b982c2d619f97fa6c877fda1
Serhii Brukhtii <brukhtii.sergey@gmail.com>
2024-06-05 16:23:27 +0300

    test request on Heroku

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/getHtml', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    const htmlContent = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Node.js Service</title>
+      </head>
+      <body>
+          <h1>Welcome to my Node.js Service</h1>
+          <p>This is a simple HTML response from a Node.js service.</p>
+      </body>
+      </html>
+    `;
+    res.send(htmlContent);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
