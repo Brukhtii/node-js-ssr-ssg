760ca66ba4224bf5fde21c64e49f0d3883069dc1
Serhii Brukhtii <sergey.brukhtii@gmail.com>
2024-06-09 01:20:45 +0300

    Add test SSG request

--- index.js
+++ index.js
@@ -44,32 +44,21 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/getHtml', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    const htmlContent = `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Node.js Service</title>
-      </head>
-      <body>
-          <h1>Welcome to my Node.js Service</h1>
-          <p>This is a simple HTML response from a Node.js service.</p>
-      </body>
-      </html>
-    `;
-    res.send(htmlContent);
-  })
   .use(cors())
   .use(express.json())
   .post('/getSSR', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     const componentsQty = req.body.componentsQty;
 
-    console.log(req.body);
     const componentBlock = getComponentsBlock({ componentsQty });
     res.send(componentBlock);
-})
+  })
+  // .use(cors())
+  // .use(express.json())
+  .post('/getSSG', (req, res) => {
+    const componentsQty = req.body.componentsQty;
+
+    const file = `assets/generatedBuild/${componentsQty}.html`;
+    res.sendFile(file, {root: __dirname })
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
