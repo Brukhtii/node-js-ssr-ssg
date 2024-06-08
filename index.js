b41c1c50c08f64ceaf3b09bf44fd53404a849359
Serhii Brukhtii <sergey.brukhtii@gmail.com>
2024-06-08 23:20:48 +0300

    add SSR post requet

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = process.env.PORT || 5001
+const imagesLinks = require('./assets/imageLinks').imagesLinks;
+
+const PORT = process.env.PORT || 5001;
+
+const getComponentsBlock = ({ componentsQty }) => {
+  const getComponent = (index) => 
+    `<div class="item" key={${index}}>
+      <div class="item-image">
+        <img src="${imagesLinks[index % 10]}" alt="Item Name"/>
+        <div class="item-actions">
+          <button type="button" class="add-to-cart-btn">Add to Cart</button>
+        </div>
+      </div>
+      <div class="item-details">
+        <h3 class="item-title">Item Title: ${index + 1}</h3>
+        <p class="item-description">This is a brief description of the item, highlighting its key features and benefits.</p>
+        <Link to="https://hidden-refuge-97632-934ef58eadcb.herokuapp.com">More info ...</Link>
+        <div class="item-meta">
+          <span class="item-price">$19.99</span>
+          <span class="item-rating">Rating: ★★★★☆</span>
+        </div>
+      </div>
+  </div>`;
+
+    const getComponents = () => {
+      const arrayWithObjects = Array.from({ length: componentsQty }, (_, index) => (getComponent(index)));
+      return arrayWithObjects;
+    }
+
+  return (
+      `<div class="components-block">
+      <h1 class="font-weight-light components-qty">Components quantity: ${componentsQty}</h1>
+        <div id="components" class="components">
+          ${getComponents()}
+        </div>
+      </div>`
+  );
+};
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -26,4 +63,13 @@ express()
     `;
     res.send(htmlContent);
   })
+  .use(express.json())
+  .post('/getSSR', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    const componentsQty = req.body.componentsQty;
+
+    console.log(req.body);
+    const componentBlock = getComponentsBlock({ componentsQty });
+    res.send(componentBlock);
+})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
